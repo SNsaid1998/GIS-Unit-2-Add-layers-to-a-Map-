8a7293eb468d0feaea8193e57ec05ec08d8f9fb9
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-02-17 18:52:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ require([
   "esri/views/MapView",
   
   "esri/widgets/BasemapToggle",
-  "esri/widgets/BasemapGallery"
+  "esri/widgets/BasemapGallery",
+   "esri/layers/FeatureLayer"
 
-], function (esriConfig, Map, MapView, BasemapToggle, BasemapGallery) {
+], function (esriConfig, Map, MapView, BasemapToggle, BasemapGallery, FeatureLayer) {
 
 esriConfig.apiKey =  "AAPKdc25ad7513a24c46b92cd024b53471173ysex4xHWQwBw2IQ-kNnn5MYzM2Maqnuc7YbUYeHblfF461bYrN1hiCHw4m9SK0E"; 
 
@@ -20,6 +21,27 @@ const view = new MapView({
           center: [-118.805, 34.027], // Longitude, latitude
           zoom: 13, // Zoom level
         });
+  
+  //Trailheads feature layer (points)
+  const trailheadsLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0"
+  });
+  
+  map.add(trailheadsLayer);
+  
+  //Trails feature layer (lines)
+  const trailsLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails_Styled/FeatureServer/0"
+  });
+  
+  map.add(trailsLayer, 0);
+  
+  // Parks and open spaces (polygons)
+  const parksLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space_Styled/FeatureServer/0"
+  });
+  
+  map.add(parksLayer, 0);
   
   const basemapToggle = new BasemapToggle({
         view: view,
